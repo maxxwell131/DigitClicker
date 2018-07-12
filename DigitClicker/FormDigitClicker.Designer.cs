9b0888fe0d221107e4d8e33554c1e71f61ae50a2
maxxwell131 <maxxwell131@yahoo.com>
2018-07-12 13:54:22 +0300

    Add project files.

---
+++ DigitClicker/FormDigitClicker.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace DigitClicker
+{
+    partial class FormDigitClicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDigitClicker));
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRepeat = new System.Windows.Forms.Button();
+            this.tableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBoxX.ForeColor = System.Drawing.Color.Black;
+            this.textBoxX.Location = new System.Drawing.Point(12, 12);
+            this.textBoxX.Multiline = true;
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.ReadOnly = true;
+            this.textBoxX.Size = new System.Drawing.Size(100, 377);
+            this.textBoxX.TabIndex = 0;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBoxY.ForeColor = System.Drawing.Color.Black;
+            this.textBoxY.Location = new System.Drawing.Point(132, 12);
+            this.textBoxY.Multiline = true;
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.ReadOnly = true;
+            this.textBoxY.Size = new System.Drawing.Size(100, 377);
+            this.textBoxY.TabIndex = 1;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonReset.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonReset.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonReset.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonReset.Location = new System.Drawing.Point(8, 8);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(110, 122);
+            this.buttonReset.TabIndex = 2;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = false;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 1;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Controls.Add(this.buttonRepeat, 0, 2);
+            this.tableLayoutPanel.Controls.Add(this.buttonAdd, 0, 1);
+            this.tableLayoutPanel.Controls.Add(this.buttonReset, 0, 0);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(238, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.Padding = new System.Windows.Forms.Padding(5);
+            this.tableLayoutPanel.RowCount = 3;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(126, 397);
+            this.tableLayoutPanel.TabIndex = 3;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.Aqua;
+            this.buttonAdd.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonAdd.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(8, 136);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(110, 122);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Add the coordinates";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonRepeat
+            // 
+            this.buttonRepeat.BackColor = System.Drawing.Color.Lime;
+            this.buttonRepeat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonRepeat.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRepeat.Location = new System.Drawing.Point(8, 291);
+            this.buttonRepeat.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.buttonRepeat.Name = "buttonRepeat";
+            this.buttonRepeat.Size = new System.Drawing.Size(110, 98);
+            this.buttonRepeat.TabIndex = 4;
+            this.buttonRepeat.Text = "Run 9 times";
+            this.buttonRepeat.UseVisualStyleBackColor = false;
+            this.buttonRepeat.Click += new System.EventHandler(this.buttonRepeat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 397);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Clicker";
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.Button buttonRepeat;
+        private System.Windows.Forms.Button buttonAdd;
+    }
+}
+
---
+++ DigitClicker/FormDigitClicker.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace DigitClicker
+{
+    public partial class FormDigitClicker : Form
+    {
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+        
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public FormDigitClicker()
+        {
+            InitializeComponent();
+            Process.Start("D:\\[C#]\\Project_s\\Digits\\Digits\\bin\\Debug\\Digits.exe");
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            textBoxX.Text = "";
+            textBoxY.Text = "";
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            int mouseX, mouseY;
+
+            mouseX = Cursor.Position.X;
+            mouseY = Cursor.Position.Y;
+
+            textBoxX.Text += mouseX.ToString() + Environment.NewLine;
+            textBoxY.Text += mouseY.ToString() + Environment.NewLine;
+
+        }
+
+        public void DoMouseClick( int x, int y)
+        {
+            Cursor.Position = new System.Drawing.Point( x, y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+        }
+
+        private void buttonRepeat_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            for (int j = 0; j < 9; j++)
+            {
+                for (int i = 0; i < textBoxX.Lines.Length; i++)
+                {
+                    x = Convert.ToInt16(textBoxX.Lines[0]);
+                    y = Convert.ToInt16(textBoxY.Lines[0]);
+
+                    DoMouseClick(x, y);
+                }
+            }
+        }
+    }
+}
